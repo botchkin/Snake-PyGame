54ddab4cef5ef647aa793879ad604c79634973be
Botchkin <Botchkin@gmail.com>
2021-11-05 07:42:21 -0600

    ensure apple gets placed inside the current screen size and randomize the initial location

--- 1_snake_game/snake_game.py
+++ 1_snake_game/snake_game.py
@@ -14,16 +14,18 @@ class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load("resources/apple.jpg").convert()
-        self.x = 120
-        self.y = 120
+        random.seed (time.time ())
+        self.x = int (random.random() * MAX/SIZE) * SIZE
+        self.y = int (random.random() * MAX/SIZE) * SIZE
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
     def move(self):
-        self.x = random.randint(1,24)*SIZE
-        self.y = random.randint(1,19)*SIZE
+        random.seed (time.time ())
+        self.x = int (random.random() * MAX/SIZE) * SIZE
+        self.y = int (random.random() * MAX/SIZE) * SIZE
 
 class Snake:
     def __init__(self, parent_screen):
